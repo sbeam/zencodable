38fe7108d252254f7bf1ebeee42affbc24d2f085
Sam Beam <sbeam@onsetcorps.net>
2011-12-01 22:48:55 -0500

    attempt at a rake task that could help update the AWS policy
    * policy (not ACL) change to the target bucket is needed so Zencoder can place your files there
    * but aws-s3 is not capable of updating the policy from a bunch of JSON - it just grants ACLs
    * you can open AWS console and paste in the policy manually. not so bad.

--- lib/tasks/zencodable_tasks.rake
+++ lib/tasks/zencodable_tasks.rake
@@ -1,4 +1,17 @@
-# desc "Explaining what the task does"
-# task :zencodable do
-#   # Task goes here
-# end
+require 'aws/s3'
+
+namespace :zencoder do
+  desc "adds a policy to your S3 bucket that should allow Zencoder to place its output files there"
+  task :add_s3_policy, :s3_config_file do |t, args|
+    s3_config_file = args[:s3_config_file] || "#{Rails.root}/config/s3.yml"
+    s3_config = YAML.load_file(s3_config_file)[Rails.env].symbolize_keys
+
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => s3_config[:access_key_id],
+      :secret_access_key => s3_config[:secret_access_key]
+    )
+    bucket = s3_config[:bucket]
+    # now if only we could update the policy (not the ACL) here, this might work
+    # AWS::S3::Bucket.find(bucket).acl.grants << somegrant
+  end
+end
--- test/zencodable_tasks_test.rb
+++ test/zencodable_tasks_test.rb
@@ -1,11 +1,31 @@
 require "rails"
 require "rails/test_help"
 require 'rake'
-load File.expand_path("../../lib/tasks/zencodable_tasks.rake",  __FILE__)
+require 'aws/s3'
 
 
 class ZencodableTaskTest < ActiveSupport::TestCase
   test "it adds the correct bucket policy to the configured bucket" do
+    @task_name = "app:options:refresh"
+
+    YAML.expects(:load_file).with('/path/to/config/amazon_s3.yml').returns({"development" => { "access_key_id" => 'ABASBDSBA', 
+                                                                                               "secret_access_key" => '99999999999', 
+                                                                                               "bucket" => 'notabucket'}})
+    AWS::S3::Base.expects(:establish_connection!).with(:access_key_id => 'ABASBDSBA', :secret_access_key => '99999999999')
+
+    # now we could update the policy (not the ACL) if that was possible
+
+    execute_rake('zencodable_tasks.rake', 'zencoder:add_s3_policy', '/path/to/config/amazon_s3.yml')
   end
+
+  def execute_rake(file, task, *args)
+    require 'rake'
+    rake = Rake::Application.new
+    Rake.application = rake
+    Rake::Task.define_task(:environment)
+    load File.expand_path("../../lib/tasks/zencodable_tasks.rake",  __FILE__)
+    rake[task].invoke(*args)
+  end
+
 end
 
